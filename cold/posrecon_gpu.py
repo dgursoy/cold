75ae6973c431bfc65839f25f4585c45226c60944
linked-liszt <anoxymoren@gmail.com>
2023-01-17 09:18:22 -0600

    Fix memory conflict

--- cold/posrecon_gpu.py
+++ cold/posrecon_gpu.py
@@ -6,6 +6,7 @@ import math
 class PosReconGPU():
     # Recommeneded 128-512, always multiple of 32. Seems good for A100
     CUDA_THREADS = 256
+    MIN_CUDA_THREADS = 32
 
     def __init__(self, batch_size, min_data_size, mask_size):
         self.min_data_size = min_data_size
@@ -14,31 +15,29 @@ class PosReconGPU():
                                                                          self.CUDA_THREADS)
 
     def calc_batch(self, mask_stack, data_stack, data_sizes):
-        #TODO Add size safety
 
         batch_size = mask_stack.shape[0]
         mask_size = mask_stack.shape[1]
 
         argmin_red_arrs = []
 
+        stream = cuda.stream()
+
         #for red_size in self.argmin_inters:
         #argmin_red_arrs.append(cuda.device_array((batch_size, 1, 2)))
         for red_size in self.argmin_inters:
-            argmin_red_arrs.append(cuda.device_array((batch_size, red_size, 2)))
-
-        print(f"Conv Blocks: {self.conv_blocks} Threads: {self.conv_block_threads}")
+            argmin_red_arrs.append(cuda.device_array((batch_size, red_size, 2), stream=stream))
 
         # NOTE: Original code skips last check. To enable set convs.shape[1] + 1
         #       and reconfigure launch parameters
         # NOTE: Flatten 3d array to 2d due to potential nuba error: https://github.com/rapidsai/cudf/issues/2710
         convs = cuda.device_array((batch_size, self.conv_size, 2))
-        self.conv_sum_kernel[self.conv_blocks, self.conv_block_threads](
+        self.conv_sum_kernel[self.conv_blocks, self.conv_block_threads, stream](
             mask_stack,
             data_stack, 
             data_sizes, 
             convs)
 
-        
         for i in range(len(self.argmin_blocks)):
             if i == 0:
                 input_arr = convs
@@ -47,11 +46,12 @@ class PosReconGPU():
 
             kern_block = (self.argmin_blocks[i], batch_size)
             thread_block = self.argmin_threads[i]
-            print(f'red {kern_block} {thread_block}')
-            self.argmin_kernel[kern_block, thread_block](
+            self.argmin_kernel[kern_block, thread_block, stream](
                 input_arr, 
                 argmin_red_arrs[i]
             )
+        
+        stream.synchronize()
 
         return argmin_red_arrs[-1].copy_to_host()
 
@@ -69,17 +69,18 @@ class PosReconGPU():
         
         intermediate_size = conv_size
         while intermediate_size * self.CUDA_THREADS > self.CUDA_THREADS:
-            # Min thread size per block = 32
-            if intermediate_size < self.CUDA_THREADS:
-                self.argmin_threads.append(int(math.ceil(intermediate_size / 32) * 32))
-            else:
+            if intermediate_size >= self.CUDA_THREADS:
                 self.argmin_threads.append(self.CUDA_THREADS)
+            elif intermediate_size <= self.MIN_CUDA_THREADS:
+                self.argmin_threads.append(self.MIN_CUDA_THREADS)
+            else:
+                # Round to nearest power of 2
+                self.argmin_threads.append(1<<(intermediate_size-1).bit_length())
+
             self.argmin_blocks.append(math.ceil(intermediate_size/self.argmin_threads[-1]))
 
             intermediate_size = math.ceil(intermediate_size / self.CUDA_THREADS)
             self.argmin_inters.append(intermediate_size)
-
-        print(f'int sizes {self.argmin_inters}, threads {self.argmin_threads} blocks {self.argmin_blocks}')
 
     def _calc_conv_sum_params(self, batch_size, min_data_size, mask_size):
         conv_total_threads = (mask_size - min_data_size) * batch_size
@@ -109,37 +110,47 @@ class PosReconGPU():
                     output[px, mask_pos, 1] = kernel_sum
                 else:
                     output[px, mask_pos, 0] = -1
+                    output[px, mask_pos, 1] = -1
             
-
         @cuda.jit
         def argmin_kernel(convs, output):
             pos_x = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
             pos_y = cuda.threadIdx.y + cuda.blockIdx.y * cuda.blockDim.y
+            tid = cuda.threadIdx.x
             
             # Block-shared reduction array. 0: Ind, 1: Value
             blk_mem = cuda.shared.array(shape=(argmin_block_size, 2), dtype=numba.float32)
+            cuda.syncthreads()
+
             if pos_x < convs.shape[1]:
-                blk_mem[cuda.threadIdx.x, 0] = convs[pos_y, pos_x, 0]
-                blk_mem[cuda.threadIdx.x, 1] = convs[pos_y, pos_x, 1]
+                blk_mem[tid, 0] = convs[pos_y, pos_x, 0]
+                blk_mem[tid, 1] = convs[pos_y, pos_x, 1]
             else:
-                blk_mem[cuda.threadIdx.x, 0] = -1
-                blk_mem[cuda.threadIdx.x, 1] = -1
+                blk_mem[tid, 0] = -1
+                blk_mem[tid, 1] = -1
 
-            conv_offset = 1
-            while conv_offset < cuda.blockDim.x:
-                if (blk_mem[cuda.threadIdx.x, 0] != -1 # skip if ignored
-                    and cuda.threadIdx.x + conv_offset < argmin_block_size # skip if oob
-                    and blk_mem[cuda.threadIdx.x + conv_offset, 0] != -1 # skip if partner is ignored
-                    and blk_mem[cuda.threadIdx.x, 1] > blk_mem[cuda.threadIdx.x + conv_offset, 1]): # value check
+            cuda.syncthreads()
 
-                    blk_mem[cuda.threadIdx.x, 0] = blk_mem[cuda.threadIdx.x + conv_offset, 0]
-                    blk_mem[cuda.threadIdx.x, 1] = blk_mem[cuda.threadIdx.x + conv_offset, 1]
+            # Blockdim must equal 2 ^ x
+            conv_offset = int(cuda.blockDim.x / 2)
+            while conv_offset > 0:
+                if (blk_mem[tid, 0] != -1 # skip if ignored
+                    and tid + conv_offset < argmin_block_size # skip if oob
+                    and pos_x + conv_offset < convs.shape[1] # skip if global oob
+                    and blk_mem[tid + conv_offset, 0] != -1 # skip if partner is ignored
+                    and blk_mem[tid, 1] > blk_mem[tid + conv_offset, 1]): # value check
                     
-                conv_offset *= 2
+                    blk_mem[tid, 0] = blk_mem[tid + conv_offset, 0]
+                    blk_mem[tid, 1] = blk_mem[tid + conv_offset, 1]
+
+                conv_offset = int(conv_offset / 2)
                 cuda.syncthreads()
+
+            cuda.syncthreads() # Required because of branch prediction?
 
             if cuda.threadIdx.x == 0:
                 output[pos_y, cuda.blockIdx.x, 0] = blk_mem[0, 0]
                 output[pos_y, cuda.blockIdx.x, 1] = blk_mem[0, 1]
+
 
         return  conv_sum_kernel, argmin_kernel
